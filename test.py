c1a769d1196e5fec30cd2e4953ef170c0686b5b1
hlx <1249534078@qq.com>
2021-04-08 16:22:35 +0800

    finish

--- test.py
+++ test.py
@@ -1,21 +1,41 @@
+def process_address(s):
+    bin_address = ""
+    temp = ""
+    i = 0
+    while i < len(s):
+        if "0" <= s[i] <= "9":
+            temp += s[i]
+        elif s[i] == '.':
+            bin_address += '{:08b}'.format(int(temp))
+            temp = ""
+        i += 1
+    bin_address += '{:08b}'.format(int(temp))
+    return bin_address
+
+
 def process(s):
-    temp = s.split(" ")
-    address = None
-    c = None
-    for i in temp:
-        if i != "" and address is None:
-            address = i
-        elif i != "" and address is not None:
-            c = i
-            break
-    temp = address.split(".")
-    bin_add = ""
-    for num in temp:
-        bin_add += '{:08b}'.format(int(num))
-    return bin_add, c
+    bin_address = ""
+    temp = ""
+    i = 0
+    while i < len(s):
+        if "0" <= s[i] <= "9" or s[i] == '.':
+            temp += s[i]
+        else:
+            return temp
+        i += 1
 
 
-s = "1.3.0.255   24 12345"
-a, b = process(s)
-print(a)
-print(b)
+f = open("1000k.txt", "r")
+fo = open("test111.txt", "w")
+lines = f.readlines()
+fo.write("[")
+i = 0
+count = 0
+for line in lines:
+    if i % 1000 == 79 and count < 100:
+        fo.write("\"" + process(line) + "\", ")
+        count += 1
+    i += 1
+fo.write("]\n")
+fo.close()
+f.close()
